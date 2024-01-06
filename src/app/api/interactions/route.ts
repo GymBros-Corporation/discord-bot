6adae4c7aceefc08af109487618650c1cc8252ee
Andy Yu <andy.yu8588@gmail.com>
2024-01-06 18:34:07 -0500

    use fetch instead of axios

--- src/app/api/interactions/route.ts
+++ src/app/api/interactions/route.ts
@@ -164,9 +164,14 @@ export async function POST(request: Request) {
         })
 
       case commands.weeklyreport.name:
-        // await axios.post("https://discord-server-muddy-mountain-5164.fly.dev/weeklyreport")
-        // const responseContent = "Generating metrics, please wait..."
-        const responseContent = await getWeeklyReport()
+        await fetch("https://discord-server-muddy-mountain-5164.fly.dev/weeklyreport", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+        const responseContent = "Generating metrics, please wait..."
+        // const responseContent = await getWeeklyReport()
 
         return NextResponse.json({
           type: InteractionResponseType.ChannelMessageWithSource,
